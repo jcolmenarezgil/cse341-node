9f45b9a017035acab64ba40e050f2f5538057649
jcolmenarezgil <jcolmenarezgil@byupathway.edu>
2025-11-01 17:40:21 -0400

    first assignment complete

--- controller/lesson1.js
+++ controller/lesson1.js
@@ -2,11 +2,16 @@ const mainRoute = (req, res) => {
     res.send('Main Route');
 };
 
+const aboutRoute = (req, res) => {
+    res.send('About Route');
+}
+
 const infoRoute = (req, res) => {
     res.send('Info Route');
 };
 
 module.exports = {
     mainRoute,
+    aboutRoute,
     infoRoute,
 }
--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 // express web server 
 var express = require('express');
 var app = express();
-const routerLesso1 = require('./controller/lesson1') 
-
-app.get('/', routerLesso1.mainRoute);
-app.get('/info', routerLesso1.infoRoute);
 
 const port = 3000;
+
+app.use('/', require('./routes'));
 
 app.listen(process.env.PORT || port);
 console.log('Web server is listening at port ' + (process.env.PORT || port));
