debd5a95d668118730c9766efab2620c621b02f6
jcolmenarezgil <jcolmenarezgil@byupathway.edu>
2025-11-01 22:33:14 -0400

    MongoDB Connection is Complete!

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var express = require('express');
+var mongodb = require('./database');
 var app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => {
-    console.log(`Running on port ${port}`)
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => { console.log(`Database is listening and node server is Running on port ${port}`) });
+    }
 });
