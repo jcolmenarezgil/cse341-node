658e6ed0acbb0d8f459b9c0d5f6e148c005f9f30
jcolmenarezgil <jcolmenarezgil@byupathway.edu>
2025-11-08 19:50:03 -0400

    Project contacts w02 mongoDB headers

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
         'Access-Control-Allow-Methods',
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
     );
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
-})
+}) */
 app.use('/', require('./routes'));
 
 mongodb.initDb((err) => {
