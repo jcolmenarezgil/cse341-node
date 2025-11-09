9378bdcc91f35ffa619bd78e06f3bcb90b6bf83d
jcolmenarezgil <jcolmenarezgil@byupathway.edu>
2025-11-08 20:09:42 -0400

    Project contacts w02 port not listening

--- server.js
+++ server.js
@@ -6,15 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-/* app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'   
-    );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
-}) */
+
 app.use('/', require('./routes'));
 
 mongodb.initDb((err) => {
