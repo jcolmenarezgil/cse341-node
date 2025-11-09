07afb0529c5dd07b8e26839d177e23db45b6d780
jcolmenarezgil <jcolmenarezgil@byupathway.edu>
2025-11-08 20:25:08 -0400

    Project contacts w02 swagger on server.js

--- routes/swagger.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const swaggerUI = require('swagger-ui-express');
-const swaggerFile = require('../swagger_output.json');
-
-router.use('./api-docs', swaggerUI.serve);
-router.get('./api-docs', swaggerUI.setup(swaggerFile));
-
-module.exports = router;
--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const bodyParser = require('body-parser');
 const mongodb = require('./database');
 const app = express();
 
+const swaggerUI = require('swagger-ui-express');
+const swaggerFile = require('./swagger_output.json');
+
+
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
 app.use('/', require('./routes'));
+app.use('./api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 mongodb.initDb((err) => {
     if (err) {
