b8e745da454a444b6ed327dff2eec4798ad187ff
jcolmenarezgil <jcolmenarezgil@byupathway.edu>
2025-11-15 15:23:19 -0400

    adding swagger.json

---
+++ swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+    info: {
+        title: 'CRUD API',
+        description: 'Crud API for CSE341',
+    },
+    host: 'https://cse341-node-6l61.onrender.com',
+    schemes: ['https', 'host']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js', './routes/movies.js'];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
