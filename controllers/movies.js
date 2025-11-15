4142579751bba3e127f3460befc4fa932a56b20d
jcolmenarezgil <jcolmenarezgil@byupathway.edu>
2025-11-15 19:09:20 -0400

    CORS solved, put & delete endpoints

--- controllers/movies.js
+++ controllers/movies.js
@@ -44,8 +44,47 @@ const createMovie = async (req, res) => {
     }
 };
 
+const updateMovie = async (req, res) => {
+    const movieId = new ObjectId(req.params.id);
+    const movie = {
+        title: req.body.title,
+        original_title: req.body.original_title,
+        year: req.body.year,
+        director: req.body.director,
+        writers: req.body.writers,
+        producers: req.body.producers,
+        cinematographer: req.body.cinematographer,
+        editor: req.body.editor,
+        music_composer: req.body.music_composer,
+        release_date: req.body.release_date,
+        running_time: req.body.running_time,
+        budget: req.body.budget,
+        rating: req.body.rating,
+        synopsis: req.body.synopsis,
+        wiki_link: req.body.wiki_link
+    };
+    const response = await mongodb.getDatabase().collection('movies').replaceOne({ _id: movieId }, movie);
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while updating the movie.');
+    }
+};
+
+const deleteMovie = async (req, res) => {
+    const movieId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().collection('movies').deleteOne({ _id: movieId });
+    if (response.deletedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while deleting the movie.');
+    }
+};
+
 module.exports = {
     getAllMovies,
     getMovieById,
-    createMovie
+    createMovie,
+    updateMovie,
+    deleteMovie
 }
--- routes/movies.js
+++ routes/movies.js
@@ -9,4 +9,8 @@ router.get('/:id', moviesController.getMovieById);
 
 router.post('/', moviesController.createMovie);
 
+router.put('/:id', moviesController.updateMovie);
+
+router.delete('/:id', moviesController.deleteMovie);
+
 module.exports = router;
