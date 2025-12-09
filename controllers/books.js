83282efe1428c229453d64a833207d008325a0f4
jcolmenarezgil <jcolmenarezgil@byupathway.edu>
2025-12-09 13:05:43 -0400

    Error Handling for book and movies

--- controllers/books.js
+++ controllers/books.js
@@ -1,21 +1,32 @@
 const mongodb = require('../database');
 const ObjectId = require('mongodb').ObjectId;
 
-const getAllBooks = async (req, res) => {
-    const result = await mongodb.getDatabase().collection('books').find();
-    result.toArray().then((books) => {
+const getAllBooks = async (req, res, next) => {
+    try {
+        const result = await mongodb.getDatabase().collection('books').find();
+        const books = await result.toArray();
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(books);
-    });
+    } catch (error) {
+        error.status = 500;
+        next(error);
+    }
 };
 
-const getBookById = async (req, res) => {
-    const bookId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().collection('books').find({ _id: bookId });
-    result.toArray().then((books) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(books[0]);
-    });
+const getBookById = async (req, res, next) => {
+    try {
+        const bookId = new ObjectId(req.params.id);
+        const result = await mongodb.getDatabase().collection('books').findOne({ _id: bookId });
+        if (result) {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(result);
+        } else {
+            res.status(404).json({ message: 'Book not found.' });
+        }
+    } catch (error) {
+        error.status = 500;
+        next(error);
+    }
 };
 
 const createBook = async (req, res, next) => {
@@ -81,13 +92,18 @@ const updateBook = async (req, res, next) => {
     }
 };
 
-const deleteBook = async (req, res) => {
-    const bookId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().collection('books').deleteOne({ _id: bookId });
-    if (response.deletedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while deleting the book.');
+const deleteBook = async (req, res, next) => {
+    try {
+        const bookId = new ObjectId(req.params.id);
+        const response = await mongodb.getDatabase().collection('books').deleteOne({ _id: bookId });
+        if (response.deletedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(404).json({ message: 'The book with the provided ID was not found for deletion.' });
+        }
+    } catch (error) {
+        error.status = 500;
+        next(error);
     }
 };
 
--- controllers/movies.js
+++ controllers/movies.js
@@ -1,25 +1,32 @@
 const mongodb = require('../database');
 const ObjectId = require('mongodb').ObjectId;
 
-const getAllMovies = async (req, res) => {
-    const result = await mongodb.getDatabase().collection('movies').find();
-    result.toArray().then((movies) => {
+const getAllMovies = async (req, res, next) => {
+    try {
+        const result = await mongodb.getDatabase().collection('movies').find();
+        const movies = await result.toArray();
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(movies);
-    });
+    } catch (error) {
+        error.status = 500;
+        next(error);
+    }
 };
 
-const getMovieById = async (req, res) => {
-    if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json('Must use a valid movie id to find a movie');
-        return;
+const getMovieById = async (req, res, next) => {
+    try {
+        const movieId = new ObjectId(req.params.id);
+        const result = await mongodb.getDatabase().collection('movies').findOne({ _id: movieId });
+        if (result) {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(result);
+        } else {
+            res.status(404).json({ message: 'Movie not found.' });
+        }
+    } catch (error) {
+        error.status = 500;
+        next(error);
     }
-    const movieId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().collection('movies').find({ _id: movieId });
-    result.toArray().then((movies) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(movies[0]);
-    });
 };
 
 
@@ -54,13 +61,6 @@ const createMovie = async (req, res, next) => {
 };
 
 const updateMovie = async (req, res, next) => {
-
-    if (!ObjectId.isValid(req.params.id)) {
-        const error = new Error('You MUST use a valid movie ID to update.');
-        error.status = 400;
-        return next(error);
-    }
-
     try {
         const movieId = new ObjectId(req.params.id);
         const movie = {
@@ -94,17 +94,18 @@ const updateMovie = async (req, res, next) => {
     }
 };
 
-const deleteMovie = async (req, res) => {
-    if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json('Must use a valid movie id to delete a movie');
-        return;
-    }
-    const movieId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().collection('movies').deleteOne({ _id: movieId });
-    if (response.deletedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while deleting the movie.');
+const deleteMovie = async (req, res, next) => {
+    try {
+        const movieId = new ObjectId(req.params.id);
+        const response = await mongodb.getDatabase().collection('movies').deleteOne({ _id: movieId });
+        if (response.deletedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(404).json({ message: 'The movie with the provided ID was not found for deletion.' });
+        }
+    } catch (error) {
+        error.status = 500;
+        next(error);
     }
 };
 
